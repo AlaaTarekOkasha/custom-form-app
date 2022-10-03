c50b7d228edc220f6750d20782501b87bf5a41a1
Alaa Tarek <57590026+AlaaTarekOkasha@users.noreply.github.com>
2022-10-03 16:54:31 +0200

    Update forms to include Phone Number Only

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -4,24 +4,16 @@ from django.utils.translation import gettext_lazy as _
 import logging
 
 
-
-
-      
-      
 class ExtraInfoForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['nationality'].required = True
-        self.fields['age'].required = True
         self.fields['phone_number'].required = True
-       
         
-
     class Meta(object):
         model = ExtraInfo
-        fields = ('nationality','age','phone_number', )
-        labels = {'nationality': _("Nationality"),'age': _("Age"),'phone_number': _("Phone number"),}
-        help_text = {'nationality': _("Please enter your Nationality"),'age': _("Please enter your Age"),'phone_number': _("Please enter your phone number"),}
+        fields = ('phone_number',)
+        labels = {'phone_number': _("Phone number"),}
+        help_text = {'phone_number': _("Please enter your phone number"),}
